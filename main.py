3ae14e2d1c8f011399125d9c4a78e78315432202
Mysticmike60t <caden.finkelstein@cttech.org>
2023-11-25 22:09:31 -0500

    sd

--- main.py
+++ main.py
@@ -2,19 +2,11 @@ import string
 import secrets
 
 
-def generate_password(length=64):
+def generate_password(length=100):
     # Define the characters that will be used in the password
     all_characters = string.ascii_letters + string.digits + string.punctuation
-    # Ensure the password includes at least one character from each category
-    password = [
-        secrets.choice(string.ascii_uppercase),
-        secrets.choice(string.ascii_lowercase),
-        secrets.choice(string.digits),
-        secrets.choice(string.punctuation)
-    ]
-    # Fill the rest of the password length with random characters
-    for i in range(length - 4):
-        password.append(secrets.choice(all_characters))
+    # Fill the password length with random characters
+    password = [secrets.choice(all_characters) for _ in range(length)]
     # Shuffle the list of characters
     secrets.SystemRandom().shuffle(password)
     # Convert the list of characters into a string
@@ -22,7 +14,7 @@ def generate_password(length=64):
     return password
 
 
-# Generate a 64-character password
+# Generate a 100-character password
 print(generate_password())
 
 # Keep the script running
